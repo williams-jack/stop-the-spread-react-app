1e2056f7bc471d10a82dea285a13927e88d19cd4
Jackson Williams <williams.jack@northeastern.edu>
2021-09-19 02:54:37 -0400

    Added stubs for business services (location services specifically). Added constants for service function use.

---
+++ src/services/business-services/locations-service.js
@@ -0,0 +1,16 @@
+// Add a loction
+const addLocation = () => {
+    // Use axios to make call to back end
+};
+// Edit a location
+const editLocation = () => {
+    // Use axios to make call to back end
+};
+// Delete a location
+const deleteLocation = () => {
+    // Use axios to make call to back end
+};
+// Get locations
+const getLocations = () => {
+    // Use axios to make call to back end
+};
---
+++ src/services/service-constants.js
@@ -0,0 +1,4 @@
+// Variables used by most or all service functions.
+const backendURL = "http://localhost:5000";
+
+export { backendURL };
